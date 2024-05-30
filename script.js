063218402f27e8e05be22ff27b223eb44688730e
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-05-30 11:22:57 -0230

    changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let currentLine = 1;
+// I originally tried using JQuery to create a typewriter effect so that letters would appear one by one, but it was convoluted and didn't really have a great effect.
+// Sometimes the best solutions are just the simplest ones.
+
+ let currentLine = 1;
 
 document.addEventListener('keydown', function() {
     const nextLine = document.getElementById(`line${currentLine}`);
